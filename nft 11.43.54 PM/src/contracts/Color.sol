75bbea5a5f4e95270d86a6401282f0d84e254c02
wilmar000 <55262109+wilmar000@users.noreply.github.com>
2020-02-25 00:38:48 -0500

    Carga de nft 1

---
+++ nft 11.43.54 PM/src/contracts/Color.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.5.16;
+
+import "./ERC721Full.sol";
+
+contract Color is ERC721Full {
+    string[] public colors;
+    mapping(string => bool) _colorExists;
+
+    constructor() public ERC721Full("Color", "COLOR") {}
+
+    // E.G. color = "#FFFFFF"
+    function mint(string memory _color) public {
+        require(!_colorExists[_color]);
+        uint256 _id = colors.push(_color);
+        _mint(msg.sender, _id);
+        _colorExists[_color] = true;
+    }
+
+}
